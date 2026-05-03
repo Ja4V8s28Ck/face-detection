c228bd657a5b67648a8622ab4f2beab3c31d0360
Ja4V8s28Ck <66721411+Ja4V8s28Ck@users.noreply.github.com>
2026-05-03 11:37:53 +0530

    feat: intialize database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
+from app.database import engine, Base
 from app.routes import router
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
     title="Real-Time Face Detection Video Streaming System",
     description="Video face detection with ROI bounding boxes",
